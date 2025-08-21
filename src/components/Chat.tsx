00c34c65ce07486d9e17f2a3c6c1373635b56176
Darkblaze30 <juanchiquiza30@gmail.com>
2025-08-20 19:02:08 -0500

    feat: :zap: First view - chat

---
+++ src/components/Chat.tsx
@@ -0,0 +1,10 @@
+import Conversation from "./Conversation"
+
+
+export default function Chat() {
+  return (
+    <main className="w-[70%] p-6 flex flex-col justify-end space-y-4">
+         <Conversation/>
+    </main>
+  )
+}
