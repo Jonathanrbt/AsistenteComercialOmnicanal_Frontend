4fba60f143c06857d0e05fac55ec590c974a8b0f
Darkblaze30 <juanchiquiza30@gmail.com>
2025-08-25 07:21:45 -0500

    feat: :sparkles: Creacion y conexion del login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes , Route} from 'react-router-dom'
 import ChatBot from './pages/Chatbot'
 import './App.css'
 import AdminPanel from './pages/AdminView'
+import LoginPage from './pages/Login'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path='/chatbot' element={<ChatBot/>}/>
       <Route path='/admin' element={<AdminPanel/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
     </Routes>
   )
 }
